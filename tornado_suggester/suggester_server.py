408c3ee9e941a2b0a7a64c2368148da4c70e5b6b
Bipul Karnani <bipul.karnani@housing.com>
2016-05-28 18:16:13 +0530

    making it more robust

--- tornado_suggester/suggester_server.py
+++ tornado_suggester/suggester_server.py
@@ -7,13 +7,18 @@ class MainHandler(tornado.web.RequestHandler):
 
     def get(self):
         query = self.get_argument('query')
-        size = self.get_argument('size', 5)
-        query_obj = QueryBuilder(query, size)
-        results = query_obj.fetch_result()['hits']['hits']
-        data = []
-        for result in results:
-            data.append({'cin': result['_id'], 'name': result['_source']['name']})
-        self.write(json.dumps(data))
+
+        if (query is None) or (len(query.strip()) == 0):
+            self.set_status(400)
+            self.write(json.dumps('bad request'))
+        else:
+            size = self.get_argument('size', 5)
+            query_obj = QueryBuilder(query, size)
+            results = query_obj.fetch_result()['hits']['hits']
+            data = []
+            for result in results:
+                data.append({'cin': result['_id'], 'name': result['_source']['name']})
+            self.write(json.dumps(data))
 
 if __name__ == "__main__":
     application = tornado.web.Application([
